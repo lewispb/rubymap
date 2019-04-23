ebe2b5b4d0f4bf2c971a2b75cf955077a89fd2a7
Lewis Buckley <lewis@lewisbuckley.co.uk>
2019-04-23 23:04:59 +0100

    Move micromodal back to webpack

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,23 +1,8 @@
-/* eslint no-console:0 */
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-//
-// To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
-// layout file, like app/views/layouts/application.html.erb
-
-
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
-
 require('./stylesheets/application.scss')
-const $ = require('jquery')
-const loadGoogleMapsApi = require('load-google-maps-api')
+
+var $ = require('jquery')
+var loadGoogleMapsApi = require('load-google-maps-api')
+var MicroModal = require('micromodal/dist/micromodal')
 
 function initializeMap() {
   loadGoogleMapsApi({ key: process.env.GOOGLE_MAPS_API_KEY }).then(function (googleMaps) {
@@ -60,3 +45,6 @@ function initializeMap() {
 }
 
 $(document).ready(initializeMap);
+$(document).ready(function() {
+  MicroModal.init();
+});
